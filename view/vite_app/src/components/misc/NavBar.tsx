85d0c3f2f77dffdccb0cbc1d8037247042fe69a2
Ranjot Dharni <ranjotdharni1@gmail.com>
2024-08-08 09:27:37 -0700

    collapsible navbar

--- view/vite_app/src/components/misc/NavBar.tsx
+++ view/vite_app/src/components/misc/NavBar.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { MouseEvent, useContext, useState } from 'react'
 import styles from '../../assets/css/misc/navBar.module.css'
 import CSS from 'csstype'
 import { Context } from '../../pages/Layout'
@@ -8,16 +8,22 @@ import flagIcon from '../../assets/img/png/flag.png'
 import rocketIcon from '../../assets/img/png/rocket.png'
 import toolsIcon from '../../assets/img/png/tools.png'
 import { useNavigate } from 'react-router-dom'
+import { IoMenuSharp } from 'react-icons/io5'
 
 export default function Navbar() {
     const redirect = useNavigate()
-    // @ts-ignore Ignore unused setTheme
-    const [selectedTheme, selectedMode, grabTheme] = useContext(Context)
+    const [selectedTheme, selectedMode] = useContext(Context)
+    const [isOpen, setOpen] = useState<boolean>(false)
 
     const inlineStyles: {[key: string]: CSS.Properties} = {
         "mainContainer": {
             background: themes[selectedMode][selectedTheme].boxGradient,
             backdropFilter: themes[selectedMode][selectedTheme].backgroundBlur,
+            border: themes[selectedMode][selectedTheme].glassBorder
+        },
+        "toggle": {
+            background: themes[selectedMode][selectedTheme].boxGradient,
+            boxShadow: themes[selectedMode][selectedTheme].glassShadow,
             border: themes[selectedMode][selectedTheme].glassBorder
         }
     }
@@ -30,15 +36,31 @@ export default function Navbar() {
         )
     }
 
+    function openNav(e: MouseEvent<HTMLButtonElement>) {
+        e.preventDefault()
+
+        setOpen(true)
+    }
+
+    document.addEventListener('click', (e: globalThis.MouseEvent) => {
+        if (isOpen && e.clientX > 60)
+            setOpen(false)
+    })
+
     // Keep Nav Icons in order of their tooltiptext set in related CSS file
     return (
-        <div className={styles.mainContainer} style={inlineStyles.mainContainer}>
-            <div className={styles.linkContainer}>
-                <NavIcon src={statsIcon} link='/metrics' />
-                <NavIcon src={flagIcon} link='/teams' />
-                <NavIcon src={rocketIcon} link='/project/launch' />
-                <NavIcon src={toolsIcon} link='/settings' />
+        <>
+            <button onClick={openNav} className={styles.toggle} style={inlineStyles.toggle} tabIndex={0}>
+                <IoMenuSharp size={18} color={themes[selectedMode][selectedTheme].primary.subheader} />
+            </button>
+            <div className={[styles.mainContainer, isOpen ? styles.open : ''].join(' ')} style={inlineStyles.mainContainer}>
+                <div className={styles.linkContainer}>
+                    <NavIcon src={statsIcon} link='/metrics' />
+                    <NavIcon src={flagIcon} link='/teams' />
+                    <NavIcon src={rocketIcon} link='/project/launch' />
+                    <NavIcon src={toolsIcon} link='/settings' />
+                </div>
             </div>
-        </div>
+        </>
     )
 }
